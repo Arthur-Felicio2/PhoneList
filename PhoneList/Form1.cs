173ad6a0cb029f77cc3c827bdde008040bf8a757
ra2457042 <ra2457042@LDI308>
2025-04-25 07:44:02 -0300

    teste

--- PhoneList/Form1.cs
+++ PhoneList/Form1.cs
@@ -12,25 +12,49 @@ namespace PhoneList
 {
     public partial class Form1 : Form
     {
-        string[,] lista;
+        string[][] lista;
         readonly int max = 100;
-        int itens = 0;
         public Form1()
         {
             InitializeComponent();
-            lista = new string[max, 2];
+            lista = new string[max][];
+        }
+        int Length(string[][] e)
+        {
+            int itens = 0;
+            for (int i = 0; i < e.Length; i++)
+            {
+                if (e[i] != null)
+                {
+                    itens++;
+                }
+            }
+            return itens;
+        }
+
+        int Length(string[] e)
+        {
+            int itens = 0;
+            for (int i = 0; i < e.Length; i++)
+            {
+                if (e[i] != null)
+                {
+                    itens++;
+                }
+            }
+            return itens;
         }
 
         void Update()
         {
             dgvList.Rows.Clear();
-            for (int i = 0;i<itens; i++)
+            for (int i = 0;i<Length(lista); i++)
             {
                 DataGridViewRow row = new DataGridViewRow();
                 row.CreateCells(dgvList);
-                for (int j = 0; j < 2; j++)
+                for (int j = 0; j < Length(lista[i]); j++)
                 {
-                    row.Cells[j].Value = lista[i, j];
+                    row.Cells[j].Value = lista[i][j];
                 }
                 dgvList.Rows.Add(row);
             }
@@ -49,30 +73,33 @@ namespace PhoneList
                 MessageBox.Show("Insira nome e telefone!");
                 return;
             }
-            lista[itens, 0] = txtName.Text;
-            lista[itens, 1] = txtPhone.Text;
-            itens++;
+            int id = 1;
+            if(Length(lista) > 0)
+            {
+                id = int.Parse(lista[Length(lista) - 1][0])+1;
+            }
+            lista[Length(lista)] = new string[] {id.ToString(), txtName.Text,txtPhone.Text};
 
             Update();
         }
 
         private void btRemove_Click(object sender, EventArgs e)
         {
-            DialogResult result = MessageBox.Show("Deseja realmente remover?", "Remover", MessageBoxButtons.YesNo);
+            DataGridViewCell cell = dgvList.SelectedCells[0];
+            int linha = cell.RowIndex;
+            string id = dgvList.Rows[linha].Cells[0].Value.ToString();
+            int indice = 0;
+            for (indice = 0; indice < Length(lista) && lista[indice][0] != id; indice++) ;
+            DialogResult result = MessageBox.Show("Deseja realmente remover?", "", MessageBoxButtons.YesNo);
             if (result == DialogResult.Yes)
             {
-                DataGridViewCell cell = dgvList.SelectedCells[0];
-                int indice = cell.RowIndex;
-                if (dgvList.SelectedCells.Count > 0)
+                for(int i = indice; i < Length(lista) - 1; i++)
                 {
-                    for (int i = indice; i < itens - 1; i++)
-                    {
-                        lista[i, 0] = lista[i + 1, 0];
-                        lista[i, 1] = lista[i + 1, 1];
-                    }
-                    itens--;
-                    Update();
+                    lista[i] = lista[i+1];
                 }
+                lista[Length(lista) - 1] = null;
+                Update();
+
             }
             else
             {
